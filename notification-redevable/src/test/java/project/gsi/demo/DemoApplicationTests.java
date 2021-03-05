9d7132d4e3d9fdc2d605080a9ea4a60dbf69b029
rida-benzaidan <79755598+rida-benzaidan@users.noreply.github.com>
2021-03-05 16:37:34 +0100

    Add files via upload

---
+++ notification-redevable/src/test/java/project/gsi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.gsi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
