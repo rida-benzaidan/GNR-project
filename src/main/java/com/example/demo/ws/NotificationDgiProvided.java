7c5cdb38ae4cf2071e9ecc588afe5249e14160ad
Your Name <you@example.com>
2021-03-07 22:21:32 +0100

    rename package Service to service

--- src/main/java/com/example/demo/ws/NotificationDgiProvided.java
+++ src/main/java/com/example/demo/ws/NotificationDgiProvided.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.Service.NotificationDgiService;
+import com.example.demo.service.NotificationDgiService;
 import com.example.demo.bean.NotificationDgi;
 
 @RestController
